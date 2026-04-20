259ab1c4bcd96e51fb65d7e299b07d862cad7a19
JHud <219741840+JamHudson@users.noreply.github.com>
2026-04-19 20:17:27 -0700

    maxDiff function

--- src/Practice.java
+++ src/Practice.java
@@ -6,15 +6,23 @@ public class Practice {
      * @return the difference between the largest and smallest number
      */
     public static int maxDiff(int[] nums) {
-        // TODO: implement this
-        return -1;
+        // Initialize the greatest and smallest number as the first number in the array.
+        int greatest = nums[0];
+        int smallest = nums[0];
+
+        // Find the greatest number in the array, find the smallest number in the array
+        for (int i=1;i<nums.length;i++) {
+            int num = nums[i];
+            if      (num > greatest) greatest = num;
+            else if (num < smallest) smallest = num;
+        }
+        // Return the difference between the greatest and smallest number
+        return (greatest-smallest);
     }
 
 
     // TODO: Implement the other methods from the README AND tests for each one
 
-    // Find the difference between the smallest and largest integer in an array
-    
     // Find the longest word that starts with a specific character letter an ArrayList
 
     // Count how many words are longer than n characters and shorter than m characters in a HashSet
