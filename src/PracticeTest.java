1614cba4b5e4bc2ea68d3284711721b448c66b3f
JHud <219741840+JamHudson@users.noreply.github.com>
2026-04-19 21:08:01 -0700

    maxDiff tests

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 public class PracticeTest {
 
-    // TODO: Make more tests for maxDiff
     @Test
     void testMaxDiffPositiveAndNegative() {
         // Arrange
@@ -17,7 +16,34 @@ public class PracticeTest {
         // Largest: 9, Smallest: -5, Difference: 9 - -5 = 14
         assertEquals(14, actual);
     }
-    
+
+    @Test
+    void testMaxDiffSingleNumber() {
+        // Arrange
+        int[] numbers = {1};
+        // Act
+        int actual = Practice.maxDiff(numbers);
+        // Assert
+        assertEquals(0,actual);
+    }
+
+    @Test
+    void testMaxDiffMultipleGreatest() {
+        int[] numbers = {0,1, 4, 2, 8, 8, 8};
+
+        int actual = Practice.maxDiff(numbers);
+
+        assertEquals(8,actual);
+    }
+
+    @Test
+    void testMaxDiffMultipleSmallest() {
+        int[] numbers = {0,0,0,3,0,2,3,2,5,2,4,12};
+
+        int actual = Practice.maxDiff(numbers);
+
+        assertEquals(12,actual);
+    }
 
     // TODO: Make tests for each problem you solve
     
